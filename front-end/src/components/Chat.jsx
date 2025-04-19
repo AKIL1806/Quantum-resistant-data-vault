86acea03c30d32be462b6f73849b2e5ef7dfa1d9
AKIL1806 <akilchowdary696@gmail.com>
2025-04-19 15:36:46 +0530

    Connect front-end with back-end for AES encryption and decryption

---
+++ front-end/src/components/Chat.jsx
@@ -0,0 +1,13 @@
+// src/components/Chat.jsx
+import React from 'react';
+
+function Chat() {
+  return (
+    <div>
+      <h2>Chat Interface Placeholder</h2>
+      <p>Here, we will integrate the chat functionality.</p>
+    </div>
+  );
+}
+
+export default Chat;
---
+++ front-end/src/components/Home.jsx
@@ -0,0 +1,13 @@
+// src/components/Home.jsx
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Quantum-Resistant Data Vault!</h1>
+      <p>Explore secure data vaulting and encryption techniques.</p>
+    </div>
+  );
+}
+
+export default Home;
